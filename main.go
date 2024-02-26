edd2dbb3212e8e60c012613602824284edf71228
Ubuntu <ubuntu@ip-172-31-5-219.us-east-2.compute.internal>
2024-02-25 21:44:16 -0600

    remove prof

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/csv"
 	"io/fs"
 	"os"
-	"runtime/pprof"
 	"sync"
 	"time"
 )
@@ -22,12 +21,6 @@ var (
 const DATA_DIR = "/home/ubuntu/data/"
 
 func main() {
-	prof, errs := os.Create("myprogram.prof")
-	if errs != nil {
-		panic(errs)
-	}
-	pprof.StartCPUProfile(prof)
-	defer pprof.StopCPUProfile()
 	fChan := make(chan fs.DirEntry)
 
 	db = InitDatabase()
