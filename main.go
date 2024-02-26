be8e66c2979289f4d9f6c5851586f75b99a29398
Colton Mosier <coltonmosier44@gmail.com>
2024-02-25 22:43:27 -0600

    fix db write?

--- main.go
+++ main.go
@@ -95,13 +95,11 @@ func fileToDb(i int, f chan fs.DirEntry) {
 		d = append(d, data)
 
 		// Insert the data into the database
-		if len(d) >= 500 {
-			WriteDeviceData(d)
-			d = nil
-			count += len(d)
-		}
 	}
 
+	WriteDeviceData(d)
+
+	count = len(d)
 	// Log the time for the queries in file
 	elapsed := time.Since(begin)
 	InfoLog.Println("Time for thread ", i, ": ", elapsed)
